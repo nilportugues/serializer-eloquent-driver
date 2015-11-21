8b9b34fc722ea439f30f8ba34dd8e1e8c51bb94c
Nil Portugués Calderó <nilopc@gmail.com>
2015-11-21 16:58:53 +0100

    First working version

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Serialize Eloquent ORM Driver
+# Serializer Driver for Eloquent ORM Models
 
 ## What?
 This is a driver for the [Serializer](https://github.com/nilportugues/serializer) library caring of Eloquent ORM model serialization.
@@ -7,3 +7,47 @@ This is a driver for the [Serializer](https://github.com/nilportugues/serializer
 Due to the popularity of Eloquent, specially in Laravel apps, a driver has been develop to unite all Eloquent serialization edge-cases for [Serializer](https://github.com/nilportugues/serializer) in a single library.
 
 
+## Installation
+
+Use [Composer](https://getcomposer.org) to install the package:
+
+```json
+$ composer require nilportugues/serializer-eloquent
+```
+
+## Usage
+
+This will return an array following the [Serializer](https://github.com/nilportugues/serializer) format.
+
+```php
+use NilPortugues\Serializer\Drivers\Eloquent\EloquentDriver;
+
+$serialized = EloquentDriver::serialize($value);
+```
+
+
+## Contribute
+
+Contributions to the package are always welcome!
+
+* Report any bugs or issues you find on the [issue tracker](https://github.com/nilportugues/serializer-eloquent-driver/issues/new).
+* You can grab the source code at the package's [Git repository](https://github.com/nilportugues/serializer-eloquent-driver).
+
+
+## Support
+
+Get in touch with me using one of the following means:
+
+ - Emailing me at <contact@nilportugues.com>
+ - Opening an [Issue](https://github.com/nilportugues/serializer-eloquent-driver/issues/new)
+
+
+
+## Authors
+
+* [Nil Portugués Calderó](http://nilportugues.com)
+* [The Community Contributors](https://github.com/nilportugues/serializer-eloquent-driver/graphs/contributors)
+
+
+## License
+The code base is licensed under the [MIT license](LICENSE).
---
+++ src/Driver.php
@@ -0,0 +1,137 @@
+<?php
+/**
+ * Author: Nil Portugués Calderó <contact@nilportugues.com>
+ * Date: 11/21/15
+ * Time: 3:44 PM
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace NilPortugues\Serializer\Drivers\Eloquent;
+
+use NilPortugues\Serializer\Serializer;
+use ErrorException;
+use Illuminate\Database\Eloquent\Model;
+use ReflectionClass;
+use ReflectionMethod;
+
+/**
+ * Class Driver
+ * @package NilPortugues\Laravel5\Serializer
+ */
+class Driver extends Serializer
+{
+    /**
+     *
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * @param mixed $value
+     *
+     * @return mixed|string
+     */
+    public function serialize($value)
+    {
+        $this->reset();
+        return $this->serializeData($value);
+    }
+
+    /**
+     * Extract the data from an object.
+     *
+     * @param mixed $value
+     *
+     * @return array
+     */
+    protected function serializeObject($value)
+    {
+        if ($value instanceof \Illuminate\Database\Eloquent\Collection) {
+            $items = [];
+            foreach ($value as &$v) {
+                $items[] = $this->serializeObject($v);
+            }
+
+            return [self::MAP_TYPE => 'array', self::SCALAR_VALUE => $items];
+        }
+
+        if ($value instanceof \Illuminate\Contracts\Pagination\Paginator) {
+            $items = [];
+            foreach ($value->items() as &$v) {
+                $items[] = $this->serializeObject($v);
+            }
+
+            return [self::MAP_TYPE => 'array', self::SCALAR_VALUE => $items];
+        }
+
+        if (\is_subclass_of($value, Model::class, true)) {
+            $stdClass = (object) $value->getAttributes();
+            $data = $this->serializeData($stdClass);
+            $data[self::CLASS_IDENTIFIER_KEY] = \get_class($value);
+
+            $methods = $this->getRelationshipAsPropertyName($value, get_class($value), new ReflectionClass($value));
+
+            if (!empty($methods)) {
+                $data = \array_merge($data, $methods);
+            }
+
+            return $data;
+        }
+
+        return parent::serializeObject($value);
+    }
+
+    /**
+     * @param                 $value
+     * @param string          $className
+     * @param ReflectionClass $reflection
+     *
+     * @return array
+     */
+    protected function getRelationshipAsPropertyName($value, $className, ReflectionClass $reflection)
+    {
+        $methods = [];
+        foreach ($reflection->getMethods(ReflectionMethod::IS_PUBLIC) as $method) {
+            if (\ltrim($method->class, '\\') === \ltrim($className, '\\')) {
+                $name = $method->name;
+                $reflectionMethod = $reflection->getMethod($name);
+
+                // Eloquent relations do not include parameters, so we'll be filtering based on this criteria.
+                if (0 == $reflectionMethod->getNumberOfParameters()) {
+                    try {
+                        $returned = $reflectionMethod->invoke($value);
+                        //All operations (eg: boolean operations) are now filtered out.
+                        if (\is_object($returned)) {
+
+                            // Only keep those methods as properties if these are returning Eloquent relations.
+                            // But do not run the operation as it is an expensive operation.
+                            if (false !== \strpos(\get_class($returned), 'Illuminate\Database\Eloquent\Relations')) {
+                                $items = [];
+                                foreach ($returned->getResults() as $model) {
+                                    if (\is_object($model)) {
+                                        /** @var Model $model */
+                                        $stdClass = (object) $model->getAttributes();
+                                        $data = $this->serializeData($stdClass);
+                                        $data[self::CLASS_IDENTIFIER_KEY] = \get_class($model);
+
+                                        $items[] = $data;
+                                    }
+                                }
+                                if (!empty($items)) {
+                                    $methods[$name] = [self::MAP_TYPE => 'array', self::SCALAR_VALUE => $items];
+                                }
+                            }
+                        }
+                    } catch (ErrorException $e) {
+                    }
+                }
+            }
+        }
+
+        return $methods;
+    }
+} 
